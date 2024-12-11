7ae52636ee77879d7ec53358642316010c744b4b
Damian Arturo Garces Paez <guners86@hotmail.com>
2024-12-11 00:11:14 -0500

    Fix bug when send email when contact load in index page

--- js/main.js
+++ js/main.js
@@ -416,6 +416,61 @@ document.addEventListener("DOMContentLoaded", function () {
     ----------------------------------------------------------*/
     document.addEventListener("swup:contentReplaced", function () {
 
+
+         /* -------------------------------------------
+           Send email
+        ------------------------------------------- */
+
+        emailjs.init("yEBL7t5_0M1T4Krsb"); // Replace with your EmailJS user ID
+
+        var contactForm = document.getElementById('contact-form');
+
+        if (contactForm) {
+            contactForm.addEventListener('submit', function(event) {
+                event.preventDefault(); // Prevent form submission
+            
+                const name = document.getElementById('name').value;
+                const email = document.getElementById('email').value;
+                const subject = document.getElementById('subject').value;
+                const message = document.getElementById('message').value;
+    
+                const loader = document.getElementById("email-loader"); // Get the loader element
+                loader.style.display = "flex"; // Show the loader
+            
+                if (document.getElementById('consent').checked &&
+                    name !== '' &&
+                    email !== '' &&
+                    subject !== '' &&
+                    message !== '') {
+                    const templateParams = {
+                        from_name: name,
+                        from_email: email,
+                        subject: subject,
+                        message: message
+                    };
+            
+                    emailjs.send('service_9kjjemm', 'template_mdiyi2l', templateParams)
+                        .then(function(response) {
+                            document.getElementById("contact-form").reset();
+                            alert('Message sent successfully!');
+                        }, function(error) {
+                            alert('Failed to send message. Please try again later.');
+                            console.error('Failed to send message:', error);
+                        })
+                        .finally(() => {
+                            // Hide the loader
+                            loader.style.display = "none";
+                        });
+                } else {
+                    loader.style.display = "none"; // Hide the loader
+                    alert('You must consent to the processing of your data or fill in all the fields.');
+                }
+    
+                return false; // Explicitly stop default behavior for older browsers
+            });
+        }
+
+
         /* -------------------------------------------
            mobile window
         ------------------------------------------- */
